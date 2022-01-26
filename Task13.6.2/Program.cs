b37395c191a83b854f5a45c5345ae78823a20a55
Smollny <Smollny@DESKTOP-0HRFRLT>
2022-01-27 00:05:05 +0300

    Добавьте файлы проекта.

---
+++ Task13.6.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task13._6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var noPunctuationText = new string(System.IO.File.ReadAllText("text1.txt")
+                .Where(c => !char.IsPunctuation(c)).ToArray()).Split(new char[] { ' ', '\n' });
+            var listStrings = new List<string>(noPunctuationText);
+            listStrings.RemoveAll(i => i == string.Empty);
+            var result = listStrings.GroupBy(i => i)
+                .Select(i => new { Слово = i.Key, Количество = i.Count() }).OrderByDescending(i => i.Количество);
+            int counter = 0;
+            foreach (var i in result)
+            {
+                if (counter == 10) break;
+                Console.WriteLine($"Слово '{i.Слово}' встречается {i.Количество} раз");
+                counter++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
